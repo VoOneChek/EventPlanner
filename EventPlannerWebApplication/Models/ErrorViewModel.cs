f39dc4a76039f6ac9aa565c7d6c81edda72d41b5
VoOneChek <milogarov@mail.ru>
2026-02-05 15:58:03 +0400

    Create project

---
+++ EventPlannerWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EventPlannerWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
